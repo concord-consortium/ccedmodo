95f00d688febc06067401db3b1d1c84d383b9bc9
Richard Klancer <rpk@pobox.com>
2012-08-29 18:26:14 -0400

    Log install request info.

--- web.rb
+++ web.rb
@@ -1,6 +1,25 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
-  logger.info "Serving Hello, world page at /"
   "Hello, world"
+end
+
+post '/install' do
+  install = JSON.parse params['install']
+  logger.info "Install request received"
+  logger.info "  install key: #{install['install_key']}"
+  logger.info "  user token: #{install['user_token']}"
+  logger.info "  groups:"
+  install['groups'].each do |group|
+  	logger.info "    " + group
+  end
+
+  content_type :json
+  return { :status => "success" }.to_json
+end
+
+
+post '/app' do
+  halt 401, 'You are not authorized.'
 end
